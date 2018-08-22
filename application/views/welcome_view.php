d459efb688711d71fb87870a9b0913347af1c774
Daniel Alves <dan_alves96@hotmail.com>
2018-08-21 22:21:07 -0300

    subindo sistema para o github

---
+++ application/views/welcome_view.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Eventos</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="assets/semantic/dist/semantic.min.css">
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="assets/semantic/dist/semantic.min.js"></script>
+</head>
+<body>
+	<h1 align="center">Eventos</h1>
+</body>
+</html>
